cb89b40294caf11163a089fb3a06be86e9b7840e
Seth Kerr <sethmsk88@yahoo.com>
2016-05-23 14:59:55 -0400

    Initial commit

---
+++ content/homepage.php
@@ -0,0 +1,48 @@
+
+<div class="container">
+	<div class="row">
+		<div class="col-lg-9">
+			<ul id="myTabs" class="nav nav-tabs">
+				<li class="active"><a data-toggle="tab" href="#ops-tab" class="tab_title">OPS</a></li>
+				<li><a data-toggle="tab" href="#usps-tab" class="tab_title">USPS</a></li>
+				<li><a data-toggle="tab" href="#ap-tab" class="tab_title">A&amp;P</a></li>
+				<li><a data-toggle="tab" href="#exec-tab" class="tab_title">Exec</a></li>
+				<li><a data-toggle="tab" href="#fac-tab" class="tab_title">Fac</a></li>
+			</ul>
+
+			<div class="tab-content myTabs">
+				<div id="ops-tab" class="tab-pane fade in active">
+					<a href="" target="_blank">Process Steps</a><br />
+					<a href="" target="_blank">Forms</a><br />
+					<a href="" target="_blank">Checklist</a><br />
+				</div>
+
+				<div id="usps-tab" class="tab-pane fade active">
+					<a href="" target="_blank">Process Steps</a><br />
+					<a href="" target="_blank">Forms</a><br />
+					<a href="" target="_blank">Checklist</a>
+				</div>
+
+				<div id="ap-tab" class="tab-pane fade active">
+					<a href="" target="_blank">Process Steps</a><br />
+					<a href="" target="_blank">Forms</a><br />
+					<a href="" target="_blank">Checklist</a>
+				</div>
+
+				<div id="exec-tab" class="tab-pane fade active">
+					<a href="" target="_blank">Process Steps</a><br />
+					<a href="" target="_blank">Forms</a><br />
+					<a href="" target="_blank">Checklist</a>
+				</div>
+
+				<div id="fac-tab" class="tab-pane fade active">
+					<a href="" target="_blank">Process Steps</a><br />
+					<a href="" target="_blank">Forms</a><br />
+					<a href="" target="_blank">Checklist</a>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+
