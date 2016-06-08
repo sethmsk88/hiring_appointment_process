8c5b2cd1f25f0b5c69904ac462af6456bdaae922
Seth Kerr <sethmsk88@yahoo.com>
2016-06-08 15:09:55 -0400

    Generate file links on homepage

--- content/homepage.php
+++ content/homepage.php
@@ -1,3 +1,55 @@
+<?php
+	// Create complete path to a file determined by the parameters
+	function getFileLink($category, $payPlan, &$fileNames_matrix)
+	{
+		$uploads_base_dir = "./uploads/";
+		$uploads_category_dirs = array(
+			'process_steps/',
+			'checklist/',
+			'forms/'
+		);
+
+		// Only return a link to the file if a fileName exists in the matrix
+		if (is_null($fileNames_matrix[$category][$payPlan])){
+			return '#';
+		} else{
+			return $uploads_base_dir . $uploads_category_dirs[$category] . $payPlan . '/' . $fileNames_matrix[$category][$payPlan];
+		}
+	}
+
+	require_once $_SERVER['DOCUMENT_ROOT'] . '/bootstrap/apps/shared/db_connect.php';
+
+	$categories = array(0,1,2);
+	$payPlans = array('ap', 'exec', 'fac', 'ops', 'usps');
+	$fileNames_matrix = array(); // category => array(payPlan => fileName)
+
+	// Get most recently uploaded file for each Pay Plan and Category
+	$sel_file_sql = "
+		SELECT FileName
+		FROM hrodt.hiring_appt_upload_history
+		WHERE Category = ? AND PayPlan = ?
+		ORDER BY UploadDate DESC
+		LIMIT 1
+	";
+
+	if (!$stmt = $conn->prepare($sel_file_sql)){
+		echo 'Prepare failed: (' . $conn->errno . ') ' . $conn->error . '<br />';
+	} else{
+
+		// Populate fileNames matrix
+		foreach ($categories as $category){
+			foreach ($payPlans as $payPlan){
+				$stmt->bind_param("is", $category, $payPlan);
+				$stmt->execute();
+				$stmt->store_result();
+				$stmt->bind_result($fileName);
+				$stmt->fetch();
+
+				$fileNames_matrix[$category][$payPlan] = $fileName;
+			}
+		}
+	}
+?>
 
 <div class="container">
 	<div class="row">
@@ -13,19 +65,19 @@
 			<div class="tab-content myTabs">
 				<div id="ops-tab" class="tab-pane fade in active" style="line-height:2.75em;">
 					<a
-						href="./uploads/process_steps/process_steps.pdf"
+						href="<?= getFileLink(0, 'ops', $fileNames_matrix) ?>"
 						target="_blank"
 						class="btn btn-primary">
 						Process Steps
 					</a><br />
 					<a
-						href="./uploads/forms/OPS_forms.zip"
+						href="<?= getFileLink(2, 'ops', $fileNames_matrix) ?>"
 						target="_blank"
 						class="btn btn-primary">
 						Forms
 					</a><br />
 					<a
-						href="./uploads/checklist/checklist.pdf"
+						href="<?= getFileLink(1, 'ops', $fileNames_matrix) ?>"
 						target="_blank"
 						class="btn btn-primary">
 						Checklist
@@ -34,19 +86,19 @@
 
 				<div id="usps-tab" class="tab-pane fade" style="line-height:2.75em;">
 					<a
-						href=""
+						href="<?= getFileLink(0, 'usps', $fileNames_matrix) ?>"
 						target="_blank"
 						class="btn btn-primary">
 						Process Steps
 					</a><br />
 					<a
-						href=""
+						href="<?= getFileLink(2, 'usps', $fileNames_matrix) ?>"
 						target="_blank"
 						class="btn btn-primary">
 						Forms
 					</a><br />
 					<a
-						href=""
+						href="<?= getFileLink(1, 'usps', $fileNames_matrix) ?>"
 						target="_blank"
 						class="btn btn-primary">
 						Checklist
@@ -55,19 +107,19 @@
 
 				<div id="ap-tab" class="tab-pane fade" style="line-height:2.75em;">
 					<a
-						href=""
+						href="<?= getFileLink(0, 'ap', $fileNames_matrix) ?>"
 						target="_blank"
 						class="btn btn-primary">
 						Process Steps
 					</a><br />
 					<a
-						href=""
+						href="<?= getFileLink(2, 'ap', $fileNames_matrix) ?>"
 						target="_blank"
 						class="btn btn-primary">
 						Forms
 					</a><br />
 					<a
-						href=""
+						href="<?= getFileLink(1, 'ap', $fileNames_matrix) ?>"
 						target="_blank"
 						class="btn btn-primary">
 						Checklist
@@ -76,19 +128,19 @@
 
 				<div id="exec-tab" class="tab-pane fade" style="line-height:2.75em;">
 					<a
-						href=""
+						href="<?= getFileLink(0, 'exec', $fileNames_matrix) ?>"
 						target="_blank"
 						class="btn btn-primary">
 						Process Steps
 					</a><br />
 					<a
-						href=""
+						href="<?= getFileLink(2, 'exec', $fileNames_matrix) ?>"
 						target="_blank"
 						class="btn btn-primary">
 						Forms
 					</a><br />
 					<a
-						href=""
+						href="<?= getFileLink(1, 'exec', $fileNames_matrix) ?>"
 						target="_blank"
 						class="btn btn-primary">
 						Checklist
@@ -97,19 +149,19 @@
 
 				<div id="fac-tab" class="tab-pane fade" style="line-height:2.75em;">
 					<a
-						href=""
+						href="<?= getFileLink(0, 'fac', $fileNames_matrix) ?>"
 						target="_blank"
 						class="btn btn-primary">
 						Process Steps
 					</a><br />
 					<a
-						href=""
+						href="<?= getFileLink(2, 'fac', $fileNames_matrix) ?>"
 						target="_blank"
 						class="btn btn-primary">
 						Forms
 					</a><br />
 					<a
-						href=""
+						href="<?= getFileLink(1, 'fac', $fileNames_matrix) ?>"
 						target="_blank"
 						class="btn btn-primary">
 						Checklist
